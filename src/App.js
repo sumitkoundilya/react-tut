42773d020814c67e7566adaeef55d74b10b14b9a
Sumit <32324157+sumitkoundilya@users.noreply.github.com>
2024-06-19 02:01:26 +0530

    brand cursor

--- src/App.js
+++ src/App.js
@@ -23,7 +23,9 @@ function App() {
       <div>
         <Navbar expand="lg" className="bg-body-tertiary">
           <Container>
-            <Navbar.Brand href="#home">React-Tutorial</Navbar.Brand>
+            <Navbar.Brand style={{ cursor: "pointer" }} onClick={() => navigate("/")}>
+              React-Tutorial
+            </Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
               <Nav className="me-auto">
